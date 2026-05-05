92ac46e83f75bfd25d3043a8ca11429547eff25b
Haydn Johnson <haydnit@gmail.com>
2026-05-04 23:16:55 -0400

    sync

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
-python3 src/main.py "/haydz-portfolio/"
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+python3 src/main.py "/github_pages_saved/"
 
--- src/textnode.py
+++ src/textnode.py
@@ -38,7 +38,7 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.LINK:
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "", {"src": "", "alt":"" })
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
 
    # def text_node_to_html_node(text_node):
    #     if text_node.text_type not in TextType:
